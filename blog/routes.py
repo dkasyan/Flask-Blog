62ce2ca9cea233ca942ba992e92cb4baf108d722
Damian Kasyan <damian.kasyan@hotmail.com>
2020-12-20 22:57:33 +0100

    TOTENDIFF

--- blog/routes.py
+++ blog/routes.py
@@ -1,17 +1,18 @@
+import functools
+
 from faker import Faker
-from flask import redirect
-from flask import render_template, request
+from flask import render_template, request, session, flash, redirect, url_for
 
 from blog import app
 from blog.forms import EntryForm
+from blog.forms import LoginForm
 from blog.models import Entry, db
 
 
 @app.route("/")
 def index():
-    # return render_template("base.html")
     all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
-    return render_template("homepage.html", all_posts=all_posts)
+    return render_template("homepage.html", all_posts=all_posts, logged=session.get('logged_in'))
 
 
 def generate_entries(how_many=10):
@@ -36,20 +37,12 @@ def data_generation():
 @app.route("/new-post/", methods=["GET", "POST"])
 def create_entry():
     form = EntryForm()
-    errors = None
+
     if request.method == 'POST':
-        if form.validate_on_submit():
-            entry = Entry(
-                title=form.title.data,
-                body=form.body.data,
-                is_published=form.is_published.data
-            )
-            db.session.add(entry)
-            db.session.commit()
-            return redirect("/")
-        else:
-            errors = form.errors
-    return render_template("entry_form.html", form=form, errors=errors)
+        create_or_edit_entry(form=form, entry=None)
+        return redirect("/")
+
+    return render_template("entry_form.html", form=form, errors=form.errors)
 
 
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
@@ -57,11 +50,66 @@ def edit_entry(entry_id):
     entry = Entry.query.filter_by(id=entry_id).first_or_404()
     form = EntryForm(obj=entry)
     errors = None
+
+    if request.method == 'POST':
+        create_or_edit_entry(form=form, entry=entry)
+        return redirect("/")
+
+    return render_template("entry_form.html", form=form, errors=form.errors)
+
+
+def create_or_edit_entry(form, entry):
+    if form.validate_on_submit():
+        if entry is None:
+            entry = Entry(
+                title=form.title.data,
+                body=form.body.data,
+                is_published=form.is_published.data
+            )
+            db.session.add(entry)
+        else:
+            form.populate_obj(entry)
+        db.session.commit()
+
+
+def login_required(view_func):
+    @functools.wraps(view_func)
+    def check_permissions(*args, **kwargs):
+        if session.get('logged_in'):
+            return view_func(*args, **kwargs)
+        return redirect(url_for('login', next=request.path))
+
+    return check_permissions
+
+
+@login_required
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    errors = None
+    next_url = request.args.get('next')
     if request.method == 'POST':
         if form.validate_on_submit():
-            form.populate_obj(entry)
-            db.session.commit()
-            return redirect("/")
+            session['logged_in'] = True
+            session.permanent = True  # Use cookie to store session.
+            flash('You are now logged in.', 'success')
+            return redirect(next_url or url_for('index'))
         else:
             errors = form.errors
-    return render_template("entry_form.html", form=form, errors=errors)
+    return render_template("login_form.html", form=form, errors=errors)
+
+
+@login_required
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+    if request.method == 'POST':
+        session.clear()
+        flash('You are now logged out.', 'success')
+    return redirect(url_for('index'))
+
+
+@app.route("/drafts/", methods=['GET'])
+@login_required
+def list_drafts():
+    drafts = Entry.query.filter_by(is_published=False).order_by(Entry.pub_date.desc())
+    return render_template("drafts.html", drafts=drafts)
